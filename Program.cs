a4d4406ace29cecc1d0c947ee1d884101fc9b918
Vikki <vikki_ka@mail.ru>
2022-12-16 21:55:52 +0300

    init repo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Clear();
+Console.Write("Ввидите число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+int i = 2;
+
+while (i <= number1)
+{
+Console.Write(i + " ");
+i += 2;
+}
